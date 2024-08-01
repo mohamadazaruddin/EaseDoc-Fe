ff2ca036f685fa783c6810a4a797e5602e3c1b7e
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2024-08-01 20:24:55 +0530

    feat: consultant page done

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from "./login";
 export { default as LandingScreen } from "./landing-screen";
 export { default as Dashboard } from "./dashboard";
+export { default as Consultant } from "./consultant";
